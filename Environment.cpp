c67e0147066381c4bfca51cbb29792c736f68daa
Patryk Pankiewicz <patryk.pankiewicz369@gmail.com>
2018-05-26 01:53:57 +0200

    Replace vectors with map, implemented most functions, added good function comments

--- Environment.cpp
+++ Environment.cpp
@@ -1,8 +1,7 @@
 #include "Environment.h"
 
 
-const char *getUserName()
-{
+const char *Environment::getUserName() {
 	uid_t uid = geteuid();              // get effective user ID
 	struct passwd *pw = getpwuid(uid);  // get pointer to pw struct
 	if (pw)
@@ -13,8 +12,7 @@ const char *getUserName()
 	return "";
 }
 
-const char *getUserHomeDir()
-{
+const char *Environment::getUserHomeDir() {
 	uid_t uid = geteuid();
 	struct passwd *pw = getpwuid(uid);
 	if(pw)
@@ -25,33 +23,16 @@ const char *getUserHomeDir()
 	return "";
 }
 
-Environment::Environment()
-{
-	loadUserVariable();
-
-	loadHomeVariable();
-
-	loadPathVariable();
-
-	const char *homeDir = getUserHomeDir();
-	currentDir_= homeDir;
-}
-
-Environment::~Environment()
-{
-
-}
-
 void Environment::loadUserVariable() {
 	const char *userName = getUserName();
 
-	globalVariables_.push_back(std::make_unique<EnvironmentVariable>("USER", std::string(userName)));
+	variablesMap_.insert(std::make_pair(std::string("USER"), std::unique_ptr<EnvironmentVariable>(new EnvironmentVariable("USER", std::string(userName), globalVariable))));
 }
 
 void Environment::loadHomeVariable() {
 	const char *homeDir = getUserHomeDir();
 
-	globalVariables_.push_back(std::make_unique<EnvironmentVariable>("HOME", std::string(homeDir)));
+	variablesMap_.insert(std::make_pair(std::string("HOME"), std::unique_ptr<EnvironmentVariable>(new EnvironmentVariable("HOME", std::string(homeDir), globalVariable))));
 
 }
 
@@ -99,113 +80,168 @@ void Environment::loadPathVariable() {
 
 	completePathValue = pathFromProfile + pathFromEnvironment;
 
-	globalVariables_.push_back(std::make_unique<EnvironmentVariable>("PATH", completePathValue));
+	variablesMap_.insert(std::make_pair(std::string("PATH"), std::unique_ptr<EnvironmentVariable>(new EnvironmentVariable("PATH", std::string(completePathValue), globalVariable))));
+}
+
+bool Environment::variableExists(const std::string &name) const {
+	auto it = variablesMap_.find(name);
+
+	if(it!=variablesMap_.end())
+		return true;
+
+	return false;
 }
 
 bool Environment::isGlobalVariable(const std::string &name) const {
-	for(auto it = globalVariables_.begin(); it!=globalVariables_.end(); ++it)
-	{
-		if((*it)->getName() == name)
-			return true;
-	}
+	auto it = variablesMap_.find(name);
+
+	if(it!=variablesMap_.end() && it->second->isGlobal())
+		return true;
 
 	return false;
 }
 
 bool Environment::isLocalVariable(const std::string &name) const {
+	auto it = variablesMap_.find(name);
 
-	for(auto it = localVariables_.begin(); it!=localVariables_.end(); ++it)
+	if(it!=variablesMap_.end() && !it->second->isGlobal())
+		return true;
+
+	return false;
+}
+
+std::vector<std::string> Environment::getPathPieces() const {
+	unsigned int index = 0;
+	std::string tmpPath = getValue("PATH");
+	std::string tmpString;
+
+	std::vector<std::string> pathPieces;
+
+	while (index<tmpPath.length()) {
+		if(tmpPath[index] == ':')
+		{
+			pathPieces.push_back(tmpString);
+			tmpString.clear();
+		}
+		else {
+			tmpString+=tmpPath[index];
+		}
+		++index;
+	}
+
+	return pathPieces;
+}
+
+bool Environment::checkIfEnvVariableNameIsValid(const std::string name) const {
+	if(name.empty())
+		return false;
+
+	char tmp = 0;
+	unsigned int index = 0;
+
+	tmp = name[index];
+	if(!isupper(static_cast<int>(tmp)))
 	{
-		if((*it)->getName() == name)
+		return false;
+	}
+	++index;
+
+	while(index<name.length())
+	{
+		tmp = name[index];
+		if(!isupper(static_cast<int>(tmp)) && !isdigit(static_cast<int>(tmp)) && tmp!='_' )
+			return false;
+	}
+
+	return true;
+}
+
+bool Environment::searchInDir(const std::string dirName, const std::string name){
+	DIR* dir = opendir(dirName.c_str());
+
+	struct dirent * sd = readdir(dir);
+	while((sd = readdir(dir)) != NULL){
+		if(strcpy(sd->d_name, name.c_str()))
+		{
 			return true;
+		}
 	}
 
 	return false;
 }
 
-bool Environment::variableExists(const std::string &name) const {
-	return (isLocalVariable(name) || isGlobalVariable(name));
+std::string Environment::getParentDir(const std::string dirName) const{
+	if(dirName== "/") // dir / don't have parent...
+		return dirName;
+
+	if(dirName.find("/") == std::string::npos) // is just someting strange... maybe exception?
+	{
+		return dirName;
+	}
+
+	unsigned int index;
+	for(index = dirName.length(); index>=0 ; --index){
+			if(dirName[index]=='/')
+			{
+				break;
+			}
+	}
+
+	return dirName.substr(0, index);
 }
 
+Environment::Environment() {
+	loadUserVariable();
+
+	loadHomeVariable();
+
+	loadPathVariable();
+
+	const char *homeDir = getUserHomeDir();
+	currentDir_= homeDir;
+}
+
+Environment::~Environment() {
+	variablesMap_.clear();
+}
 
 void Environment::setVariable(const std::string &name, const std::string &value) {
-	if(variableExists(name))   // variable exists
-	{
-		if(isGlobalVariable(name)) // is global
-		{
-			for(auto it = globalVariables_.begin(); it!=globalVariables_.end(); ++it)
-			{
-				if((*it)->getName() == name)
-					(*it)->setValue(value);
-			}
-		}
-		else                        // is local
-		{
-			for(auto it = localVariables_.begin(); it!=localVariables_.end(); ++it)
-			{
-				if((*it)->getName() == name)
-					(*it)->setValue(value);
-			}
-		}
+	auto it = variablesMap_.find(name);
 
+	if(it!=variablesMap_.end())
+	{
+		it->second->setValue(value);
 	}
 
-	else                        // variables not exists
+	else
 	{
-		localVariables_.push_back(std::make_unique<EnvironmentVariable>(name, value));
+		variablesMap_.insert(std::make_pair(value, std::unique_ptr<EnvironmentVariable>(new EnvironmentVariable(name, value, localVariable))));
 	}
 }
 
-std::string Environment::getValue(const std::string &name)
-{
+std::string Environment::getValue(const std::string &name) const  {
+	auto it = variablesMap_.find(name);
 
-	if(variableExists(name))
-	{
-		if(isGlobalVariable(name)) // is global
-		{
-			for(auto it = globalVariables_.begin(); it!=globalVariables_.end(); ++it)
-			{
-				if((*it)->getName() == name)
-					return (*it)->getValue();
-			}
-		}
-		else                        // is local
-		{
-			for(auto it = localVariables_.begin(); it!=localVariables_.end(); ++it)
-			{
-				if((*it)->getName() == name)
-					return (*it)->getValue();
-			}
-		}
-	}
+	if(it!=variablesMap_.end())
+		return it->second->getValue();
 
 	return std::__cxx11::string();
 }
 
 void Environment::exportVariable(const std::string &name) {
-	if(isLocalVariable(name))
-	{
-		for(auto it = localVariables_.begin(); it!=localVariables_.end(); ++it)
-		{
-			if((*it)->getName() == name) {
-				globalVariables_.push_back(std::move(*it));
-				localVariables_.erase(it);
-				break;
-			}
-		}
-	}
+	auto it = variablesMap_.find(name);
+	if(it!=variablesMap_.end())
+		it->second->setGlobal();
 }
 
-
-
 char **Environment::getEnvironment() {
-	char **tmp= new char*[globalVariables_.size() + 1]; // +1 because of /0 as last one
+	char **tmp= new char*[variablesMap_.size() + 1]; // +1 because of /0 as last one
 
 	int i=0;
-	for(auto it = globalVariables_.begin(); it!=globalVariables_.end(); ++it)
+	for(auto it = variablesMap_.begin(); it!=variablesMap_.end(); ++it)
 	{
-		tmp[i] = new char[(*it)->getName().length() + (*it)->getValue().length() + 2]; // +2 because of '=' and '\0'
-		std::string tmpStr = (*it)->getName() + "=" + (*it)->getValue();
+		tmp[i] = new char[it->second->getName().length() + it->second->getValue().length() + 2]; // +2 because of '=' and '\0'
+		std::string tmpStr = it->second->getName() + "=" + it->second->getValue();
 		strcpy(tmp[i], tmpStr.c_str());
 		++i;
 	}
@@ -216,7 +252,97 @@ char **Environment::getEnvironment() {
 }
 
 std::string Environment::expandPath(const std::string &name) {
-	return std::__cxx11::string();
+	std::string toReturn = name;
+	std::string tmp;
+	int pos;
+
+	if(name == ".")
+	{
+		return currentDir_;
+	}
+
+	if(name == "..")
+	{
+		return getParentDir(currentDir_);
+	}
+
+	if(name == "~")
+	{
+		return getValue("HOME");
+	}
+
+	while( (pos = toReturn.find("~")) != std::string::npos){
+		tmp = toReturn;
+		toReturn.clear();
+		if(pos!=0)
+		{
+			toReturn = tmp.substr(0, pos-1);
+			toReturn += getValue("HOME");
+			toReturn += tmp.substr(pos+1, tmp.length());
+		}
+		else
+		{
+			toReturn = getValue("HOME");
+			toReturn += tmp.substr(1, tmp.length());
+		}
+	}
+
+	while( (pos = toReturn.find("..")) !=std::string::npos)
+	{
+		tmp = toReturn;
+		toReturn.clear();
+		if(pos!=0)
+		{
+			toReturn = tmp.substr(0, pos-1);
+			toReturn += getParentDir(currentDir_);
+			toReturn += tmp.substr(pos+2, tmp.length());
+		}
+		else
+		{
+			toReturn = getParentDir(currentDir_);
+			toReturn += tmp.substr(2, tmp.length());
+		}
+	}
+
+	while( (pos = toReturn.find("./")) != std::string::npos)
+	{
+		tmp = toReturn;
+		toReturn.clear();
+		if(pos!=0)
+		{
+			toReturn = tmp.substr(0, pos-1);
+			toReturn += currentDir_;
+			toReturn += tmp.substr(pos+1, tmp.length());
+		}
+		else
+		{
+			toReturn = currentDir_;
+			toReturn += tmp.substr(1, tmp.length());
+		}
+	}
+
+	return toReturn;
+}
+
+std::string Environment::searchPath(const std::string &name) {
+	std::vector<std::string> pathPieces = getPathPieces();
+	std::string tmp;
+
+	for(auto it = pathPieces.begin(); it!=pathPieces.end(); ++it)
+	{
+		if(searchInDir(*it, name)){
+			tmp += *it;
+			break;
+		}
+	}
+
+	if(!tmp.empty())
+	{
+		tmp+="/";
+		tmp+=name;
+	}
+
+	return tmp;
 }
 
 void Environment::setCurrentDir(const std::string &path) {
@@ -233,8 +359,4 @@ int Environment::getMask() {
 
 void Environment::setMask() {
 
-}
-
-std::string Environment::searchPath(const std::string &name) {
-	return std::__cxx11::string();
 }
--- Environment.h
+++ Environment.h
@@ -3,7 +3,6 @@
 #include "Command.h"
 
 #include <iostream>
-#include <unordered_map>
 #include <fstream>
 
 #include <unistd.h>
@@ -11,9 +10,12 @@
 #include <cstring>
 #include <sys/types.h>
 #include <pwd.h>
+#include <dirent.h>
+#include <cctype>
 
 #include <vector>
 #include <memory>
+#include <unordered_map>
 
 /**
  *	@class Environment
@@ -28,13 +30,15 @@ class Environment {
 		globalVariable
 	};
 
+
 	struct EnvironmentVariable
 	{
 		std::string name_;
 		std::string value_;
+		int variableType_;
 
-		EnvironmentVariable(const std::string name, const std::string value="")
-				:name_(name), value_(value)
+		EnvironmentVariable(const std::string name, const std::string value, const int variableType)
+				:name_(name), value_(value), variableType_(variableType)
 		{};
 
 		std::string getName() const
@@ -50,20 +54,109 @@ class Environment {
 		void setValue(const std::string newValue) {
 			value_=newValue;
 		}
+
+		void setGlobal(){
+			variableType_ = globalVariable;
+		}
+
+		bool isGlobal() const
+		{
+			if(variableType_==globalVariable)
+				return true;
+			return false;
+		}
 	};
 
+	std::unordered_map<std::string, std::unique_ptr<EnvironmentVariable>> variablesMap_;
 	std::string currentDir_;
-	std::vector<std::unique_ptr<EnvironmentVariable>> localVariables_;
-	std::vector<std::unique_ptr<EnvironmentVariable>> globalVariables_;
 
 	private:
+	/**
+	 * @brief Get actual user name by function getpwuid
+	 * @return user name
+	 */
+
+	const char *getUserName();
+
+	/**
+	 * @brief Get user home dir by function getpwuid
+	 * @return user home dir name
+	 */
+
+	const char *getUserHomeDir();
+
+	/**
+	 * @brief Insert into variables map "USER" env variable
+	 */
+
 	void loadUserVariable();
+
+	/**
+	 * @brief Insert into variables map "HOME" env variable
+	 */
+
 	void loadHomeVariable();
+
+	/**
+	 * @brief Insert into variables map "PATH" env variable
+	 */
+
 	void loadPathVariable();
 
+	/**
+	 * @brief Check if exist env variable with given name
+	 * @param name env variable name
+	 * @return true if exist, false if not exist
+	 */
+
 	bool variableExists(const std::string &name) const;
+
+	/**
+	 * @brief Check if env variable with given name is global
+	 * @param name env variable name
+	 * @return true if is global, false if not
+	 */
+
 	bool isGlobalVariable(const std::string &name) const;
+
+	/**
+	 * @brief Check if env variable with given name is local
+	 * @param name env variable name
+	 * @return trus if is global, false if is local
+	 */
+
 	bool isLocalVariable(const std::string &name) const;
+
+	/**
+	 * @brief 'Cuts' the "PATH" variable into pieces - directories names
+	 * @return vector of "PATH" pieces
+	 */
+
+	std::vector<std::string> getPathPieces() const;
+
+	/**
+	 * @brief It use dirent.h API to search in given directory file
+	 * @param dirName directory name
+	 * @param name wanted file name
+	 * @return file path or empty string if is not in that directory
+	 */
+
+	bool searchInDir(const std::string dirName, std::string name);
+
+	/**
+	 * @brief Check if given env variable name is valid with Std 1003.1-2001
+	 * @param name name to check
+	 * @return true if is valid, false if is bot valid
+	 */
+
+	bool checkIfEnvVariableNameIsValid(const std::string name ) const;
+
+	/**
+	 * @brief Get parent dir for given directory
+	 * @return parent directory name
+	 */
+
+	std::string getParentDir(const std::string dirName) const;
 
 	public:
 
@@ -72,11 +165,10 @@ class Environment {
 	 * getpwuid to ged HOME and USER
 	 */
 
-
 	Environment();
 
 	/**
-	 * @brief Destructor delete tables
+	 * @brief Destructor clear variables map
 	 */
 
 	~Environment();
@@ -86,10 +178,11 @@ class Environment {
 	 * @brief Sets the variable to the given value. If it doesn't exist,
 	 * 		  it shall be created
 	 *
-	 * @param name - must begin with a character and do not contatin
+	 * @param name - must begin with a character and do not contain
 	 * 			any special characters nor a white space
 	 * @param value - can be any C-string, can contain any ascii character, it can be empty
 	 */
+
 	void setVariable(const std::string &name, const std::string &value);
 
 	/**
@@ -97,12 +190,14 @@ class Environment {
 	 * @param name - variable name
 	 * @return variable value or empty string if the variable doesn't exist or has an empty value
 	 */
-	std::string getValue(const std::string &name);
+
+	std::string getValue(const std::string &name) const;
 
 	/**
 	 * @brief Sets a given variable to be global - visible to bash subprocesses
 	 * @param name - variable name
 	 */
+
 	void exportVariable(const std::string &name);
 
 	/**
@@ -110,6 +205,7 @@ class Environment {
 	 * 		  then passed to the exec function, when a new process is executed.
 	 * @return Null-terminated array of pointers to char*.
 	 */
+
 	char **getEnvironment();
 
 	/**
@@ -128,6 +224,7 @@ class Environment {
 	 * @return expanded path
 	 * @throws RuntimeException if the path is invalid
 	 */
+
 	std::string expandPath(const std::string &name);
 
 	/**
@@ -136,6 +233,7 @@ class Environment {
 	 * @return expanded path
 	 * @throws RuntimeException if the command does not exist
 	 */
+
 	std::string searchPath(const std::string &name);
 
 	void setCurrentDir(const std::string &path);
